1a1df2fd6a188464db0bdd335b24bff56041ea6d
Hussie <husnainkhaliq82@gmail.com>
2024-08-06 12:20:52 -0400

    changed layout

--- app/page.js
+++ app/page.js
@@ -1,15 +1,12 @@
-'use client';
-
+'use client'
 import { useState, useEffect } from 'react';
-import { Box, Button, Modal, Stack, TextField, Typography, AppBar, Toolbar, IconButton, Container, CssBaseline, ThemeProvider, Switch, Drawer, List, ListItem, ListItemText, useMediaQuery, useTheme } from '@mui/material';
+import { Box, Button, Modal, Stack, TextField, Typography, AppBar, Toolbar, IconButton, Container, CssBaseline, ThemeProvider, Drawer, List, ListItem, ListItemText, useMediaQuery, useTheme } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import { collection, deleteDoc, doc, getDoc, getDocs, query, setDoc } from 'firebase/firestore';
 import Link from 'next/link';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
-
 import { firestore } from "@/firebase";
-
 import { createTheme } from '@mui/material/styles';
 import { grey } from '@mui/material/colors';
 
@@ -31,9 +28,9 @@ export const getTheme = (mode) =>
               secondary: grey[500],
             },
             buttons: {
-              home: '#ff5722', // Custom color for Home button
-              portfolio: '#4caf50', // Custom color for Portfolio button
-              linkedin: '#0e76a8', // Custom color for LinkedIn button
+              home: '#e64a19', // Darker shade for Home button
+              portfolio: '#388e3c', // Darker shade for Portfolio button
+              linkedin: '#004c8c', // Darker shade for LinkedIn button
             }
           }
         : {
@@ -49,9 +46,9 @@ export const getTheme = (mode) =>
               secondary: grey[700],
             },
             buttons: {
-              home: '#ff5722', // Custom color for Home button
-              portfolio: '#4caf50', // Custom color for Portfolio button
-              linkedin: '#0e76a8', // Custom color for LinkedIn button
+              home: '#ff5722', // Default color for Home button
+              portfolio: '#4caf50', // Default color for Portfolio button
+              linkedin: '#0e76a8', // Default color for LinkedIn button
             }
           }),
     },
@@ -68,13 +65,16 @@ export const getTheme = (mode) =>
         styleOverrides: {
           root: ({ theme }) => ({
             '&.home': {
-              color: theme.palette.buttons.home,
+              backgroundColor: theme.palette.buttons.home,
+              color: theme.palette.getContrastText(theme.palette.buttons.home),
             },
             '&.portfolio': {
-              color: theme.palette.buttons.portfolio,
+              backgroundColor: theme.palette.buttons.portfolio,
+              color: theme.palette.getContrastText(theme.palette.buttons.portfolio),
             },
             '&.linkedin': {
-              color: theme.palette.buttons.linkedin,
+              backgroundColor: theme.palette.buttons.linkedin,
+              color: theme.palette.getContrastText(theme.palette.buttons.linkedin),
             },
           }),
         },
@@ -91,6 +91,7 @@ export const getTheme = (mode) =>
     },
   });
 
+
 export default function Home() {
   const [pantry, setPantry] = useState([]);
   const [open, setOpen] = useState(false);
@@ -98,7 +99,7 @@ export default function Home() {
   const [item, setItemName] = useState('');
   const [quantity, setQuantity] = useState('');
   const [editingItem, setEditingItem] = useState(null);
-  const [darkMode, setDarkMode] = useState(false); // State for dark mode
+  const [darkMode] = useState(true); // Dark mode only
 
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
@@ -193,9 +194,18 @@ export default function Home() {
   };
 
   return (
-    <ThemeProvider theme={getTheme(darkMode ? 'dark' : 'light')}>
+    <ThemeProvider theme={getTheme('dark')}>
       <CssBaseline />
-      <Box sx={{ flexGrow: 1 }}>
+      <Box
+        sx={{
+          flexGrow: 1,
+          backgroundImage: 'url(/wowww.jpg)', // Path from public folder
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center',
+          minHeight: '100vh',
+        }}
+      >
         <AppBar position="static" sx={{ mb: 4 }}>
           <Toolbar>
             <IconButton
@@ -204,7 +214,7 @@ export default function Home() {
               aria-label="menu"
               sx={{
                 mr: 2,
-                fontSize: '2rem', // Make the icon bigger
+                fontSize: '2rem',
                 color: 'white',
                 '&:hover': {
                   backgroundColor: 'rgba(255, 255, 255, 0.1)',
@@ -255,7 +265,6 @@ export default function Home() {
               </Box>
             </Drawer>
 
-
             {!isMobile && (
               <>
                 <Link href="/" passHref>
@@ -270,7 +279,7 @@ export default function Home() {
               </>
             )}
             <Box sx={{ flexGrow: 1 }} />
-            <Switch checked={darkMode} onChange={() => setDarkMode(!darkMode)} />
+            {/* Remove the Switch */}
           </Toolbar>
         </AppBar>
 
@@ -279,9 +288,22 @@ export default function Home() {
             <Typography variant="h4" align="center" sx={{ mb: 4 }}>
               Pantry Tracker
             </Typography>
-            <Typography variant="body1" align="center" sx={{ mb: 4 }}>
+            <Typography
+  variant="body1"
+  align="center"
+  sx={{
+    mb: 4,
+    fontFamily: 'Lora, serif',
+    fontSize: '1.6rem',
+    color: 'rgba(255, 255, 255, 0.5)', // Slightly less opaque for better readability
+    textShadow: '1px 1px 3px rgba(0, 0, 0, 0.5)', // Adds a subtle shadow to improve contrast
+  }}
+>
   It&apos;s a simple and intuitive application designed to help you keep track of the items in your pantry. With this app, you can easily add new items, update their quantities, and remove items when they&apos;re no longer needed.
 </Typography>
+
+
+
 
             <Box display="flex" gap={2} sx={{ mb: 4 }}>
               <Button variant="contained" color="primary" onClick={handleOpen}>
@@ -293,74 +315,72 @@ export default function Home() {
             </Box>
 
             <Modal open={open} onClose={handleClose}>
-  <Box
-    sx={{
-      position: 'fixed',
-      top: '50%',
-      left: '50%',
-      transform: 'translate(-50%, -50%)',
-      width: '90%',
-      maxWidth: '500px',
-      bgcolor: 'background.paper',
-      p: 4,
-      borderRadius: '12px',
-      boxShadow: 24,
-      display: 'flex',
-      flexDirection: 'column',
-      gap: 3,
-      outline: 'none',
-    }}
-  >
-    <Typography variant="h6">
-      {editingItem ? 'Update Quantity' : 'Add Item'}
-    </Typography>
-    <Stack spacing={2}>
-      <TextField
-        value={item}
-        onChange={(e) => setItemName(e.target.value)}
-        label="Item Name"
-        variant="outlined"
-        fullWidth
-        disabled={!!editingItem}
-      />
-      <TextField
-        value={quantity}
-        onChange={(e) => setQuantity(e.target.value)}
-        label="Quantity"
-        variant="outlined"
-        fullWidth
-        type="number"
-        inputProps={{ min: "1" }} // Ensure input allows only positive numbers
-      />
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => {
-          if (editingItem) {
-            if (parseInt(quantity) > 0) {
-              updateItemQuantity();
-            } else {
-              alert('Quantity must be greater than 0');
-            }
-          } else {
-            if (parseInt(quantity) > 0) {
-              addItem(item);
-              setItemName('');
-            } else {
-              alert('Quantity must be greater than 0');
-            }
-          }
-          handleClose();
-        }}
-        disabled={parseInt(quantity) <= 0} // Disable button if quantity is not valid
-      >
-        {editingItem ? 'Update' : 'Add'}
-      </Button>
-    </Stack>
-  </Box>
-</Modal>
-
-
+              <Box
+                sx={{
+                  position: 'fixed',
+                  top: '50%',
+                  left: '50%',
+                  transform: 'translate(-50%, -50%)',
+                  width: '90%',
+                  maxWidth: '500px',
+                  bgcolor: 'background.paper',
+                  p: 4,
+                  borderRadius: '12px',
+                  boxShadow: 24,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  gap: 3,
+                  outline: 'none',
+                }}
+              >
+                <Typography variant="h6">
+                  {editingItem ? 'Update Quantity' : 'Add Item'}
+                </Typography>
+                <Stack spacing={2}>
+                  <TextField
+                    value={item}
+                    onChange={(e) => setItemName(e.target.value)}
+                    label="Item Name"
+                    variant="outlined"
+                    fullWidth
+                    disabled={!!editingItem}
+                  />
+                  <TextField
+                    value={quantity}
+                    onChange={(e) => setQuantity(e.target.value)}
+                    label="Quantity"
+                    variant="outlined"
+                    fullWidth
+                    type="number"
+                    inputProps={{ min: "1" }} // Ensure input allows only positive numbers
+                  />
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => {
+                      if (editingItem) {
+                        if (parseInt(quantity) > 0) {
+                          updateItemQuantity();
+                        } else {
+                          alert('Quantity must be greater than 0');
+                        }
+                      } else {
+                        if (parseInt(quantity) > 0) {
+                          addItem(item);
+                          setItemName('');
+                        } else {
+                          alert('Quantity must be greater than 0');
+                        }
+                      }
+                      handleClose();
+                    }}
+                    disabled={parseInt(quantity) <= 0} // Disable button if quantity is not valid
+                  >
+                    {editingItem ? 'Update' : 'Add'}
+                  </Button>
+                </Stack>
+              </Box>
+            </Modal>
 
             <Box width="100%" maxWidth="800px" mt={4}>
               <Typography variant="h5" align="center" sx={{ mb: 2 }}>
